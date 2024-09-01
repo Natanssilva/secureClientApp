c6d3304be28db5274bcffb9aedd2ff19189aadd1
Natan Silva <natanssilva10@gmail.com>
2024-09-01 19:55:46 -0300

    FIX | Configurando element plus

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import './assets/main.css'
+import ElementPlus from 'element-plus'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
